171dea93f41ead94206ada7f021d4c0ba743fafe
Mukesh Kumar Patel <mukesh.kumaar8293@gmail.com>
2020-01-26 15:41:02 +0530

    Added Global headers

--- app/src/main/java/com/mukesh/retrofitsampleapp/repository/ApiConfig.java
+++ app/src/main/java/com/mukesh/retrofitsampleapp/repository/ApiConfig.java
@@ -1,9 +1,16 @@
 package com.mukesh.retrofitsampleapp.repository;
 
+import androidx.annotation.NonNull;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -30,8 +37,29 @@ public class ApiConfig {
                     //DEFAULT
                     //HEADERS etc
                     loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+         //Simple Retrofit without HEADER
+                   /* OkHttpClient okHttpClient = new OkHttpClient
+                            .Builder()
+                            .addInterceptor(loggingInterceptor)
+                            .build();
+*/
+                   //Request with Global Header
 
-                    OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+                    OkHttpClient okHttpClient = new OkHttpClient
+                            .Builder()
+                            .addInterceptor(new Interceptor() {
+                                @NonNull
+                                @Override
+                                public Response intercept(@NonNull Chain chain) throws IOException {
+                                    Request originalRequest = chain.request();
+                                    Request newRequest = originalRequest.newBuilder()
+                                            .header("Global-Header", "VALUE")
+                                            .build();
+                                    return chain.proceed(newRequest);
+                                }
+                            })
+                            .addInterceptor(loggingInterceptor)
+                            .build();
 
 
                     //This custom Gson data will ignore null values ,needs to add in to
